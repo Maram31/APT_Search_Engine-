2abe7bbe96c81a616af9d766165b13e3d4d95bf1
Maram31 <maram311999@hotmail.com>
2021-06-04 01:51:31 +0200

    Added crawler files

---
+++ Crawler/src/main/java/Main.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maram
+ */
+
+import java.util.*;
+
+import java.net.*;
+import java.io.*;
+import org.jsoup.nodes.Document;
+
+public class Main {
+    protected static Queue<String> seedsList = new LinkedList<>();
+    protected static Queue<Document> HTMLdocuments = new LinkedList<>();
+    //protected static Queue<String> collectedUrls = new LinkedList<>();          //for indexer
+
+    protected static Set<String> restrictedSites = new HashSet<>();
+    protected static Set<String> visitedUrls = new HashSet<String>();
+    //protected static List<String> visitedUrls = new ArrayList<String>();
+    
+    protected static Set<String> compactString = new HashSet<String>();
+    
+    protected static Integer visitedUrlsCount = 0;
+
+    final static int stoppingCriteria = 2000;
+    static boolean crawlerFinished = false;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Thread crawling = new Thread(new CrawlerController());
+      
+        // thread for indexer
+        readSeeds(seedsList);
+       
+        crawling.start();
+        // indexer starts
+              
+        crawling.join();
+        // indexer joins
+        
+        if (!CrawlerController.isInterrupted) {
+        }
+
+        //for (String item: seedsList) {
+            //System.out.println(item);
+        //}
+    }
+    
+    
+    
+    public static void readSeeds(Queue<String> seedsList) {
+        try {
+            File mySeeds = new File("C:\\Users\\Maram\\Documents\\GitHub\\APT_Search_Engine-\\Crawler\\src\\main\\java\\seeds.txt");
+            Scanner myReader = new Scanner(mySeeds);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                seedsList.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while reading seeds from file!");
+            e.printStackTrace();
+        }
+    } 
+
+}
